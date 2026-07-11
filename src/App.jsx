00f3e89b2833f776f627715360355f8ca35aae0a
Hiran-Joy <hiranjoy7@gmail.com>
2026-07-11 12:33:47 +0530

    Add DeleteRegistration component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import SearchRegistration from './components/SearchRegistration'
 import AddRegistration from './components/AddRegistration'
+import DeleteRegistration from './components/DeleteRegistration'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <>
       <SearchRegistration />
     <AddRegistration/>
+    <DeleteRegistration/>
     </>
   )
 }
