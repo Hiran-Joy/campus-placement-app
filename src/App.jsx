bdcd31280fd2c908f9b61068e9e21f009082c45b
Hiran-Joy <hiranjoy7@gmail.com>
2026-07-11 12:28:16 +0530

    Add AddRegistration component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import SearchRegistration from './components/SearchRegistration'
+import AddRegistration from './components/AddRegistration'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <SearchRegistration />
+    <AddRegistration/>
     </>
   )
 }
